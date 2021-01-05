02a6217bd6f1e391c208505e8057c29ef29443dc
chronoB <finn.o.bayer@gmail.com>
2021-01-05 12:25:46 +0100

    blocking parallel execution of lighthouse calls

--- runLighthouse.js
+++ runLighthouse.js
@@ -1,60 +1,89 @@
-const fs = require('fs');
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
-async function runLighthouse (website, folder) {
-  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-  const options = {output: 'json', onlyCategories: ['performance','seo','accessibility','best-practices'], port: chrome.port};
-  const runnerResult = await lighthouse(website, options);
+const fs = require("fs")
+const lighthouse = require("lighthouse")
+const chromeLauncher = require("chrome-launcher")
 
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-  let stripURL = url.strip("/")
-  fs.writeFileSync(folder+"/"+stripURL[-1]+'lhreport.json', reportHtml);
+let mutex = false
 
-  // `.lhr` is the Lighthouse Result as a JS object
-  console.log('Report is done for', runnerResult.lhr.finalUrl);
-  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
-  console.log('SEO score was', runnerResult.lhr.categories.seo.score * 100);
-  console.log('Accessibility score was', runnerResult.lhr.categories.accessibility.score * 100);
-  console.log('best-practices score was', runnerResult.lhr.categories['best-practices'].score * 100);
-    //add this to summary.json
-  await chrome.kill();
-};
-
-let curDir =""
-
-fs.readdir("urls", (err, files) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  files.forEach(file => {
-    console.log(file);
-    fs.stat("urls/"+file, (err, stats)=> {
-      if (err) {
-        console.error(err)
+async function runLighthouse(website, folder) {
+    while (mutex) {
+        setTimeout(() => {
+            runLighthouse(website, folder)
+        }, 1000)
         return
-      }
-      console.log(stats)
-      if (stats.isDirectory()){
-        let folder = "urls/"+file
-        if (file ==="vor-gaenge.de") return
-        fs.readFile(folder+"/urls.txt",'utf8',(err,data)=>{
-          if (err) {
-            console.error(err)
-            return
-          }
-          //console.log(data)
-          let lines = data.split(/\r\n|\n/)
-          lines.forEach(url => {
-            console.log(url)
-            console.log(typeof(url))
-            if (url !== "") runLighthouse(url, folder)
-          })
+    }
+    console.log("Grabbing mutex for " + website)
+    mutex = true
+    const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] })
+    const options = {
+        output: "json",
+        onlyCategories: [
+            "performance",
+            "seo",
+            "accessibility",
+            "best-practices",
+        ],
+        port: chrome.port,
+    }
+    const runnerResult = await lighthouse(website, options)
+
+    // `.report` is the HTML report as a string
+    const reportHtml = runnerResult.report
+    let stripURL = website.split("/")
+    fs.writeFileSync(folder + "/" + stripURL[-1] + "lhreport.json", reportHtml)
+
+    // `.lhr` is the Lighthouse Result as a JS object
+    console.log("Report is done for", runnerResult.lhr.finalUrl)
+    console.log(
+        "Performance score was",
+        runnerResult.lhr.categories.performance.score * 100
+    )
+    console.log("SEO score was", runnerResult.lhr.categories.seo.score * 100)
+    console.log(
+        "Accessibility score was",
+        runnerResult.lhr.categories.accessibility.score * 100
+    )
+    console.log(
+        "best-practices score was",
+        runnerResult.lhr.categories["best-practices"].score * 100
+    )
+    //add this to summary.json
+    await chrome.kill()
+    mutex = false
+}
+
+fs.readdir("urls", (errReadDir, files) => {
+    if (errReadDir) {
+        console.error(errReadDir)
+        return
+    }
+    files.forEach((file) => {
+        fs.stat("urls/" + file, (errStats, stats) => {
+            if (errStats) {
+                console.error(errStats)
+                return
+            }
+            if (stats.isDirectory()) {
+                let folder = "urls/" + file
+                if (file === "vor-gaenge.de") return
+                fs.readFile(
+                    folder + "/urls.txt",
+                    "utf8",
+                    (errReadFiles, data) => {
+                        if (errReadFiles) {
+                            console.error(errReadFiles)
+                            return
+                        }
+                        let lines = data.split(/\r\n|\n/)
+                        lines.forEach((url) => {
+                            if (url !== "") {
+                                runLighthouse(url, folder)
+                            }
+                        })
+                    }
+                )
+            }
         })
-      }
     })
-  })
 })
 
 // iterate through urls directory
@@ -64,7 +93,7 @@ fs.readdir("urls", (err, files) => {
 //  - create summary.json start
 //  - read urls.txt line by line
 //      - runLighthouse with webpage, link to folder which contains reports
-//          - run lighthouse for webpage. 
+//          - run lighthouse for webpage.
 //          - save report for website.
 //          - append summary to summary.json
 //  - close summary.json
